b2e77c7bd4d9e4534c8b5a6205646e25ae642eb3
Sam Rice <samsrice@gmail.com>
2022-08-03 10:57:43 -0700

    Add array methods and define one new array

--- arrays.js
+++ arrays.js
@@ -3,3 +3,15 @@ var myGuitars = ["Gibson J-50", "Harmony H1203", "Gibson Les Paul Standard", "Fe
 var myFavNums = [7, 27, 45, 100];
 
 var theBools = [true, false, true, true];
+
+/* The below method will remove "7" from the myFavNums array. */
+
+myFavNums.shift(0);
+
+/* The below method will add "Epiphone Sheraton" to the end of the myGuitars array. */
+
+myGuitars.push("Epiphone Sheraton");
+
+/* The below method will create a new array from the first two elements in the myGuitars array. */
+
+var myAcoustics = myGuitars.slice(0, 2);
